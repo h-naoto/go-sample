e1c75cab06e108d0d3daf72b971de8bc0d898422
Naoto Hanaue <hanaue.naoto@po.ntts.co.jp>
2014-12-02 10:30:37 +0900

    channel sample update

--- go_samples/Channel/chan_sample.go
+++ go_samples/Channel/chan_sample.go
@@ -3,45 +3,70 @@ package main
 
 import (
 	"fmt"
+	//	"launchpad.net/tomb"
 	"time"
 )
 
-const (
-	_ = iota
-	COLOR_RED
-	COLOR_BURE
-	COLOR_BLACK
-)
-
 type Car struct {
-	Name      string
-	Color     map[int]string
-	SpeedBase int
-	Gus       int
+	Name    string
+	Color   string
+	Speed   int
+	UpSpeed int
+	Gas     int
+	GasCons int
 }
 
-func NewCar() *Car {
-	car := &Car{}
-	car.Color = make(map[int]string)
+func NewCar(name string, color string, bspeed int, cas int) *Car {
+	car := &Car{
+		Name:    name,
+		Color:   color,
+		Speed:   bspeed,
+		UpSpeed: 1,
+		Gas:     cas,
+		GasCons: 2,
+	}
 	return car
 }
-func main() {
-	car := NewCar()
-	car.Name = "stinglay"
-	car.Color[COLOR_BLACK] = "black"
-	car.SpeedBase = 30
-	car.Gus = 100
 
+//var Tomb tomb.Tomb
+
+func main() {
+	car := NewCar("stinglay", "black", 30, 30)
+	ch1 := make(chan int, 1)
+	ch2 := make(chan int, 1)
 	fmt.Println("# Start #")
-	go Calculate(car)
-	time.Sleep(10 * time.Second)
+	go SpeedUp(car, ch1)
+	go OutgoGas(car, ch2)
+	for {
+		select {
+		case speed := <-ch1:
+			fmt.Println("Car Speed -> ", speed, "km")
+		case gas := <-ch2:
+			if gas < 0 {
+				return
+			}
+			fmt.Println("Car Gas -> ", gas, "L")
+		default:
+			fmt.Println("---------------------------------")
+			time.Sleep(1 * time.Second)
+		}
+	}
+	//time.Sleep(10 * time.Second)
 	fmt.Println("# End #")
 }
-func Calculate(car *Car) {
-	for i := 0; i < 1000; i++ {
-		car.SpeedBase += 1
-		car.Gus -= 2
+func SpeedUp(car *Car, ch1 chan int) {
+	for {
+		car.Speed += car.UpSpeed
+		ch1 <- car.Speed
 		time.Sleep(1 * time.Second)
-		fmt.Println(car)
+		//fmt.Println("Car Speed -> ", car.Speed, "km")
+	}
+}
+func OutgoGas(car *Car, ch2 chan int) {
+	for {
+		car.Gas -= car.GasCons
+		ch2 <- car.Gas
+		time.Sleep(1 * time.Second)
+		//fmt.Println("Car Speed -> ", car.Gas, "L")
 	}
 }
